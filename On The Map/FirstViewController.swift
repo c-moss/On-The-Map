246668f04faef0d5bab715034e62b0ffbc7a6581
cmoss <campbell.moss@fiserv.com>
2016-05-01 10:13:20 +1200

    First cut - blank tabbed app

---
+++ On The Map/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  On The Map
+//
+//  Created by Campbell Moss on 1/05/16.
+//  Copyright © 2016 Campbell Moss. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ On The Map/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  On The Map
+//
+//  Created by Campbell Moss on 1/05/16.
+//  Copyright © 2016 Campbell Moss. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
