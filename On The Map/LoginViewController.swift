a93662fdcb9e73fce73e990368f381a02e58128a
cmoss <campbell.moss@fiserv.com>
2016-05-02 15:21:56 +1200

    Add login screen

--- On The Map/LoginViewController.swift
+++ On The Map/LoginViewController.swift
@@ -1,25 +1,27 @@
 //
-//  SecondViewController.swift
+//  LoginViewController.swift
 //  On The Map
 //
-//  Created by Campbell Moss on 1/05/16.
+//  Created by Campbell Moss on 2/05/16.
 //  Copyright © 2016 Campbell Moss. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func loginPressed(sender: UIButton) {
+        performSegueWithIdentifier("login", sender: self)
+    }
+    
 }
-
---
+++ On The Map/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewController.swift
+//  On The Map
+//
+//  Created by Campbell Moss on 1/05/16.
+//  Copyright © 2016 Campbell Moss. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
