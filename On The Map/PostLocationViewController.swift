753adda0774bfbbe3308f4fd0b9d5de4f635a735
cmoss <campbell.moss@fiserv.com>
2016-05-20 15:31:20 +1200

    Add Post Location detail views and basic navigation

--- On The Map/PostLocationViewController.swift
+++ On The Map/PostLocationViewController.swift
@@ -7,17 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class PostLocationViewController: BaseViewController, UITextViewDelegate {
     
     @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
     
     @IBOutlet weak var locationPlaceholder: UILabel!
     
     @IBOutlet weak var locationTextView: UITextView!
     
+    @IBOutlet weak var locationEntryView: UIView!
+    @IBOutlet weak var locationDetailView: UIView!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewWillAppear(animated: Bool) {
         findButton.layer.cornerRadius = 10
+        submitButton.layer.cornerRadius = 10
+        
+        
+        locationEntryView.hidden = false
+        locationDetailView.hidden = true
+        locationTextView.text = ""
     }
     
     //MARK: fake placeholder for UITextView
@@ -29,4 +42,12 @@ class PostLocationViewController: BaseViewController, UITextViewDelegate {
         locationPlaceholder.hidden = textView.hasText()
     }
     
+    @IBAction func cancelButtonClicked(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func findButtonClicked(sender: UIButton) {
+        locationEntryView.hidden = true
+        locationDetailView.hidden = false
+    }
 }
