210b6981303ee8f2a74dcd0c002f1dc193342e8a
cmoss <campbell.moss@fiserv.com>
2016-05-22 08:08:38 +1200

    Add map search functionality to Post Location view

--- On The Map/PostLocationViewController.swift
+++ On The Map/PostLocationViewController.swift
@@ -11,33 +11,29 @@ import MapKit
 
 class PostLocationViewController: BaseViewController, UITextViewDelegate {
     
-    @IBOutlet weak var findButton: UIButton!
-    @IBOutlet weak var submitButton: UIButton!
-    
-    @IBOutlet weak var locationPlaceholder: UILabel!
-    
-    @IBOutlet weak var locationTextView: UITextView!
-    
+    // MARK: First location view
     @IBOutlet weak var locationEntryView: UIView!
-    @IBOutlet weak var locationDetailView: UIView!
+    @IBOutlet weak var locationTextView: UITextView!
+    @IBOutlet weak var locationPlaceholder: UILabel!
+    @IBOutlet weak var findButton: UIButton!
     
+    // MARK: Second location view
+    @IBOutlet weak var locationDetailView: UIView!
+    @IBOutlet weak var linkTextView: UITextField!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var submitButton: UIButton!
     
     override func viewWillAppear(animated: Bool) {
         findButton.layer.cornerRadius = 10
         submitButton.layer.cornerRadius = 10
         
-        
-        locationEntryView.hidden = false
-        locationDetailView.hidden = true
-        locationTextView.text = ""
+        resetUI()
     }
     
-    //MARK: fake placeholder for UITextView
+    // MARK: fake placeholder for UITextView
     func textViewDidBeginEditing(textView: UITextView) {
         locationPlaceholder.hidden = true
     }
-    
     func textViewDidEndEditing(textView: UITextView) {
         locationPlaceholder.hidden = textView.hasText()
     }
@@ -47,7 +43,53 @@ class PostLocationViewController: BaseViewController, UITextViewDelegate {
     }
     
     @IBAction func findButtonClicked(sender: UIButton) {
+        guard let searchQuery = locationTextView.text where !searchQuery.isEmpty else {
+            self.showErrorAlert(message: "Please enter a location search term")
+            return
+        }
+        
+        let mapSearchRequest = MKLocalSearchRequest()
+        mapSearchRequest.naturalLanguageQuery = searchQuery
+        
+        let mapSearch = MKLocalSearch(request: mapSearchRequest)
+        mapSearch.startWithCompletionHandler { (response, error) in
+            guard error == nil else {
+                print("Map search error: \(error)")
+                self.showErrorAlert(message: "Could not complete location search - please try again")
+                return
+            }
+            
+            guard let response = response else {
+                print("No search response returned")
+                self.showErrorAlert(message: "Could not complete location search - please try again")
+                return
+            }
+            
+            guard !response.mapItems.isEmpty else {
+                print("No search results returned")
+                self.showErrorAlert(message: "No results returned - please try again")
+                return
+            }
+            
+            self.pinMapAndZoom(response.mapItems[0])
+        }
         locationEntryView.hidden = true
         locationDetailView.hidden = false
+    }
+    
+    // Add an annotation pin to the mapView and zoom to show that pin
+    private func pinMapAndZoom(mapItem: MKMapItem) {
+        self.mapView.addAnnotation(mapItem.placemark)
+        
+        let zoomRegion = MKCoordinateRegionMake(mapItem.placemark.coordinate, MKCoordinateSpanMake(5, 5))
+        mapView.setRegion(zoomRegion, animated: false)
+    }
+    
+    // Reset the UI state
+    private func resetUI() {
+        locationEntryView.hidden = false
+        locationDetailView.hidden = true
+        locationTextView.text = ""
+        mapView.removeAnnotations(mapView.annotations)
     }
 }
